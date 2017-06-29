76ce591c658dd3a8305f26151cd5fe0119207c41
Sebastian Grillo <seba.grillo@gmail.com>
2017-06-29 20:41:25 -0300

    Added Flexbox reusable components

---
+++ src/components/Flexbox/ColumnComponent.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+interface ColumnComponentProps {
+    className?: string;
+    children?: any;
+    bgcolor?: string;
+}
+
+function ColumnComponentContainer(props: ColumnComponentProps): JSX.Element {
+    return(
+        <div className={props.className}>
+            {props.children}
+        </div>
+    );
+}
+
+const ColumnComponent = styled(ColumnComponentContainer)`    
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+
+    flex-flow: column wrap;
+    
+    background-color: ${props => props.bgcolor ? props.bgcolor : '#fefcfc'};
+    width: 100%
+    height: 100%;
+    border: none;
+`;
+
+export default ColumnComponent;
