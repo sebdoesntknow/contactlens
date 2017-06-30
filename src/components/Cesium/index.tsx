eb134729432ed0a467febca1365f21f9a9fec48c
Sebastian Grillo <seba.grillo@gmail.com>
2017-06-29 23:40:35 -0300

    Added Celsium placeholder

---
+++ src/components/Cesium/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class Cesium extends React.Component<{}, {}> {
+    render() {
+        return <div>Celsium</div>;
+    }
+}
+
+export default Cesium;
---
+++ typings/cesium/index.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class BuildModuleUrl extends React.Component<any, any> {}
+export class CesiumViewer extends React.Component<any, any> {}
+export class Entity extends React.Component<any, any> {}
+export class CesiumPatcher extends React.Component<any, any> {}
+export class Cartesian3 extends React.Component<any, any> {}
